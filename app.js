2aade492cbcd50d6a2257dd30e90dbff4175d67f
pawan-live <pawan.senpura@gmail.com>
2023-01-12 00:33:30 +0530

    add delete functionality

--- app.js
+++ app.js
@@ -1,58 +1,70 @@
 /* ---- START CONFIG ---- */
 
-// LOWDB Setup
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-
-const adapter = new FileSync("db.json");
-const db = low(adapter);
-
 var obj = {
-  table: [],
+  events: [],
 };
 
 /* ---- END CONFIG ---- */
 
-// getData();
+getData();
+
+document.querySelectorAll(".delete-event-btn").forEach((element) => {
+  console.log(element);
+  // element.addEventListener("click", function () {
+  //   let _time = element.parentNode.parentNode.parentNode.id;
+  //   deleteEvent(_time);
+  // });
+});
 
 /* ---- FUNCTIONS ---- */
 
 // get data from LOWDB database
-// function getData() {}
+function getData() {
+  // send data as GET request to server
+  fetch("http://localhost:3080/getEvents")
+    .then((res) => res.json())
+    .then((data) => {
+      obj.events = data;
+      loadTable(obj.events);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 // load table
 function loadTable(array) {
   // remove 'no events found' notice
   document.getElementById("table--empty-text").style.display = "none";
   array.forEach((element) => {
-    addRowToTable(element.name, element.id);
+    addRowToTable(element.time, element.name);
   });
 }
 
 // add row to table
-function addRowToTable(name, id) {
+function addRowToTable(time, name) {
   let tableContent = document.getElementById("table-content");
   let tableData = tableContent.innerHTML;
 
   let newTableContent =
     `
     <tr id="` +
-    id +
+    time +
     `">
         <td>` +
-    id +
+    time +
     `</td>
         <td>` +
     name +
     `</td>
         <td>
             <div class="btn-group" role="group">
-                <!-- <button class="btn btn-light link-primary"
+                <!-- <button class="edit-event-btn btn btn-light link-primary"
                     data-bs-toggle="tooltip" data-bss-tooltip="" type="button" title="Edit"
                     style="border-style: solid;border-color: var(--bs-btn-hover-border-color);">
                     <i class="fas fa-pencil-alt"></i>
                 </button> -->
-                <button class="btn btn-light link-danger" data-bs-toggle="tooltip"
+                <button class="delete-event-btn btn btn-light link-danger" data-bs-toggle="tooltip"
                     data-bss-tooltip="" type="button" title="Delete"
                     style="border-color: var(--bs-btn-hover-border-color);">
                     <i class="fas fa-trash-alt"></i>
@@ -75,11 +87,57 @@ function createNewEvent() {
 
   // validate form
   if (name == "" || time == "" || url == "") {
-    throwError("Please fill out all fields.");
+    alert("Please fill out all fields.");
     return;
   }
 
-  obj.table.push({ id: time, name: name, file: url });
+  // send data to database
+
+  // send data as POST request to server
+  fetch("http://localhost:3080/addEvent", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      time: time,
+      url: url,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      alert(data.message);
+      location.reload();
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("Error. Could not add event.");
+    });
+
+  resetForm();
+}
+
+function deleteEvent(time) {
+  // send data as POST request to server
+  fetch("http://localhost:3080/deleteEvent", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      time: time,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      alert(data.message);
+      location.reload();
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("Error. Could not delete event.");
+    });
 }
 
 function hideAlert() {
@@ -106,7 +164,7 @@ function displayTime() {
     hour: "numeric",
     minute: "numeric",
     second: "numeric",
-    hour12: true,
+    hour12: false,
   });
   document.getElementById("time_display").innerHTML = time;
 }
--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 // electron JS setup
 const { app, BrowserWindow } = require("electron");
+
+// express js setup
 const express = require("express");
 const expressApp = express();
-const fs = require("fs"); //require file system object
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const path = require("path");
-const axios = require("axios");
+
+// LOWDB Config
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ events: [], user: {} }).write();
+
+// LOWdb config end
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -33,9 +44,47 @@ app.on("window-all-closed", () => {
   }
 });
 
-// var server = expressApp.listen(3080, function () {
-//   var host = server.address().address;
-//   var port = server.address().port;
+//create an endpoint to getData from database
+expressApp.get("/getEvents", cors(), function (req, res) {
+  let events = db.get("events").value();
+  res.send(events);
+});
 
-//   console.log("Listening at http://%s:%s", host, port);
-// });
+//create an endpoint to add data to database
+expressApp.post("/addEvent", cors(), bodyParser.json(), function (req, res) {
+  let data = req.body;
+  let existingEvent = db.get("events").find({ time: data.time }).value();
+  if (existingEvent) {
+    return res.json({
+      message: "Event already exists",
+    });
+  } else {
+    db.get("events").push(data).write();
+    return res.status(200).json({
+      message: "Event added successfully",
+    });
+  }
+});
+
+//create an endpoint to delete data from database
+expressApp.post("/deleteEvent", cors(), bodyParser.json(), function (req, res) {
+  let data = req.body;
+  let existingEvent = db.get("events").find({ time: data.time }).value();
+  if (existingEvent) {
+    db.get("events").remove({ time: data.time }).write();
+    return res.status(200).json({
+      message: "Event deleted successfully",
+    });
+  } else {
+    return res.json({
+      message: "Event does not exist",
+    });
+  }
+});
+
+var server = expressApp.listen(3080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Listening at http://%s:%s", host, port);
+});
