aff6b7196c2958532ab5b4ed9fa1bf0842516935
pawan-live <pawan.senpura@gmail.com>
2023-01-15 14:37:52 +0530

    add build conf

--- forge.config.js
+++ forge.config.js
@@ -4,19 +4,10 @@ module.exports = {
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
-      config: {},
-    },
-    {
-      name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
-    },
-    {
-      name: "@electron-forge/maker-deb",
-      config: {},
-    },
-    {
-      name: "@electron-forge/maker-rpm",
-      config: {},
+      config: {
+        certificateFile: "./cert.pfx",
+        certificatePassword: process.env.CERTIFICATE_PASSWORD,
+      },
     },
   ],
 };
